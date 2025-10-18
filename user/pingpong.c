a703c53ec8c3773676d80893b526f09e70eb98a2
dc <1876397104dc@gmail.com>
2025-10-18 13:06:16 -0400

    lab1: pingpong first test

---
+++ user/pingpong.c
@@ -0,0 +1,26 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int pingpong(void){
+    int p1[2];
+    pipe(p1);
+    int p2[2];
+    pipe(p2);
+    char* buffer;
+    if(fork == 0){
+        int pid = getpid();
+        read(p1[0], buffer , 1);
+        printf("%d : received ping" , pid);
+        write(p2[1], buffer , 1);
+        exit(0);
+    }
+    else{
+        int pid = getpid();
+        write(p1[1],buffer,1);
+        read(p2[0],buffer, 1);
+        printf("%d : receive pong",pid);
+        exit(0);
+        
+    }
+}

5b1cbb155e52e449ea7ce457e3fe4c5a975b0fff
dc <1876397104dc@gmail.com>
2025-10-18 13:11:35 -0400

    lab1 : pingpong fork() is a function () need to be include classic pointer mistake

--- user/pingpong.c
+++ user/pingpong.c
@@ -7,19 +7,19 @@ int pingpong(void){
     pipe(p1);
     int p2[2];
     pipe(p2);
-    char* buffer;
-    if(fork == 0){
+    char buffer;
+    if(fork() == 0){
         int pid = getpid();
-        read(p1[0], buffer , 1);
-        printf("%d : received ping" , pid);
-        write(p2[1], buffer , 1);
+        read(p1[0], &buffer , 1);
+        printf("%d : received ping\n" , pid);
+        write(p2[1], &buffer , 1);
         exit(0);
     }
     else{
         int pid = getpid();
-        write(p1[1],buffer,1);
-        read(p2[0],buffer, 1);
-        printf("%d : receive pong",pid);
+        write(p1[1],&buffer,1);
+        read(p2[0],&buffer, 1);
+        printf("%d : receive pong\n",pid);
         exit(0);
         
     }
